d4d0665d25cd6d0f9ce4d41fa38f4d0692479428
ye-yu <rafolwen98@gmail.com>
2019-11-29 01:04:30 +0800

    Dynamically create new section in project bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 $('.link').click(e => {
   window.location = $(e.target.parentElement).attr('href');
 })
+
+var projectCategories = JSON.parse(`
+  [
+    \"Python\",
+    \"Java\",
+    \"C\",
+    \"PHP\",
+    \"JavaScript/Node.js\",
+    \"Web Design\",
+    \"Music\"
+  ]
+`);
+
+for(i of projectCategories){
+  let temp_id = i.toLowerCase().replace(" ", "-");
+  let temp_name = i;
+  let navTabTemplate = `<span class="nav-link" id="v-tabs-${temp_id}-tab" data-toggle="pill" href="#v-tabs-${temp_id}" role="tab" aria-controls="v-tabs-${temp_id}" aria-selected="false">${temp_name}</span>`;
+  $("#v-tabs-tab-projects").append(navTabTemplate);
+}
