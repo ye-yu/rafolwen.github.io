f1b56d5518b6b2877b8640b9afce79950f063c32
ye-yu <rafolwen98@gmail.com>
2019-11-29 22:24:19 +0800

    Use bootstrap rounded on hackerrank badge

--- js/hackerrank.js
+++ js/hackerrank.js
@@ -10,7 +10,7 @@ for(i of badges) {
   let name = i.name;
   let src = i.imgSrc;
   let template = `
-                    <div class="hackerrank-image py-2 my-3 mx-2 bg-white d-flex flex-column text-center justify-content-center">
+                    <div class="hackerrank-image rounded py-2 my-3 mx-2 bg-white d-flex flex-column text-center justify-content-center">
                       <img class="mx-auto pt-3" src="${src}" alt="${name}" />
                       <p class="text-xs px-3 text-dark font-weight-bold">${name}</p>
                     </div>
