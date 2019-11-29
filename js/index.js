d2bbb7eee3c12803149cac9e947230e4584f0811
ye-yu <rafolwen98@gmail.com>
2019-11-30 03:24:12 +0800

    Add border bottom + adjust spacing of project section

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ var projectCategories = JSON.parse(`
 for(i of projectCategories){
   let temp_id = i.toLowerCase().replace(" ", "-");
   let temp_name = i;
-  let navTabTemplate = `<span class="nav-link my-1 py-lg-2 py-5 text-lg-left text-center" id="v-tabs-${temp_id}-tab" data-toggle="pill" href="#v-tabs-${temp_id}" role="tab" aria-controls="v-tabs-${temp_id}" aria-selected="false">${temp_name}</span>`;
+  let navTabTemplate = `<span class="nav-link my-5 my-lg-0 py-3 text-lg-left text-center" id="v-tabs-${temp_id}-tab" data-toggle="pill" href="#v-tabs-${temp_id}" role="tab" aria-controls="v-tabs-${temp_id}" aria-selected="false">${temp_name}</span>`;
   $("#v-tabs-tab-projects").append(navTabTemplate);
 }
 
