37db48c2ca438dc8ca7c8f4266b81ed109ca716e
ye-yu <rafolwen98@gmail.com>
2019-11-30 00:01:20 +0800

    Enlarge background on badge and tooltip on mobile

--- js/hackerrank.js
+++ js/hackerrank.js
@@ -12,7 +12,7 @@ for(i of badges) {
   let template = `
                     <div class="hackerrank-image rounded-circle py-2 my-3 mx-2 bg-white d-flex flex-column text-center justify-content-center">
                       <img class="mx-auto pt-3" src="${src}" alt="${name}" />
-                      <p class="text-xs px-3 text-dark font-weight-bold">${name}</p>
+                      <p class="px-3 text-dark font-weight-bold">${name}</p>
                     </div>
                     `;
   $("#badges-section").append(template);
