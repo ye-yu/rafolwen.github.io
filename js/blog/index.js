a4b39a75e98aaff0a7217cb944c6805cb63d6f61
ye-yu <rafolwen98@gmail.com>
2019-11-30 23:40:59 +0800

    Fix reference link in js

--- js/blog/index.js
+++ js/blog/index.js
@@ -21,7 +21,7 @@ $(function () {
 // read from projects.JSON
 var projects;
 $.ajax({
-  url: "/js/blog/projects.json",
+  url: "js/blog/projects.json",
   dataType: "json",
   success: function (data) {
     projects = data;
