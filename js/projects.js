2d60577c116378a90ea60890fd2f0780b5124df7
ye-yu <rafolwen98@gmail.com>
2019-11-30 11:46:18 +0800

    Remove project placeholder

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,42 @@
 var projects;
 var mlProjects;
 var webProjects;
+
+var appendProjectTo = (projs, eid) => {
+  for(i of projs) {
+    let imgSrc = i.thumbnailSrc ? i.thumbnailSrc : "https://via.placeholder.com/150";
+    let imgCap = i.thumbnailCap ? i.thumbnailCap : "No captions provided";
+    let projTitle = i.name;
+    let projSubtitle = "Tagged under: " + i.tags.join(", ");
+    let projDesc = i.description;
+    let projLink = "/blog.html?project=" + i.hashId;
+    let template = `
+    <div class="row py-1 border-bottom">
+      <div class="col-3 border-right py-4 d-flex flex-column justify-content-center align-items-center">
+        <div>
+          <img src="${imgSrc}" style="max-width:200px; max-height:200px;" alt="${imgCap}" />
+        </div>
+        <div class="text-sm font-weight-bold">
+          ${imgCap}
+        </div>
+      </div>
+      <div class="col-9">
+        <a class="pt-3 pb-1 font-main h1 d-block text-body" href="${projLink}">
+          ${projTitle}
+        </a>
+        <div class="pt-1 pb-2 pl-2 border-bottom font-main text-muted h5">
+          ${projSubtitle}
+        </div>
+        <div class="py-3 px-2 text-body">
+          ${projDesc}
+        </div>
+      </div>
+    </div>
+
+    `;
+    $(eid).append(template);
+  }
+}
 $.ajax({
   url: "/js/blog/projects.json",
   dataType: "json",
@@ -9,5 +45,8 @@ $.ajax({
     projects = data;
     mlProjects = projects.projects.filter(x => x.category == 'Machine Learning');
     webProjects = projects.projects.filter(x => x.category == 'Web');
+
+    appendProjectTo(mlProjects, '#machine-learning-projects');
+    appendProjectTo(webProjects, '#web-apps-projects');
   }
 });
