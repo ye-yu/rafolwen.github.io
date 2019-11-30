94aed15dc1677af8cc90f6d8cf7a6cb9dbdc7ef3
ye-yu <rafolwen98@gmail.com>
2019-11-30 11:16:16 +0800

    Filter projects based on categories

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,13 @@
 // read from projects.JSON
 var projects;
+var mlProjects;
+var webProjects;
 $.ajax({
   url: "/js/blog/projects.json",
   dataType: "json",
   success: function (data) {
     projects = data;
+    mlProjects = projects.projects.filter(x => x.category == 'Machine Learning');
+    webProjects = projects.projects.filter(x => x.category == 'Web');
   }
 });
