582f51a6e9d2f0c7746ec805123e1c17411a2f21
ye-yu <rafolwen98@gmail.com>
2019-12-24 09:05:13 +0800

    Append category dynamically

--- js/blog/index.js
+++ js/blog/index.js
@@ -1,22 +1,35 @@
-var projectCategories = JSON.parse(`
-  [
-    \"Machine Learning\",
-    \"Web\",
-    \"Command Line\"
-  ]
-`);
-
-for(i of projectCategories){
-  let temp_id = i.toLowerCase().replace(" ", "-");
-  let temp_name = i;
-  let navTabTemplate = `<span class="nav-link my-5 my-lg-0 py-3 text-lg-left text-center" id="v-tabs-${temp_id}-tab" data-toggle="pill" href="#v-tabs-${temp_id}" role="tab" aria-controls="v-tabs-${temp_id}" aria-selected="false">${temp_name}</span>`;
-  $("#v-tabs-tab-projects").append(navTabTemplate);
-}
-
 // Activate all tooltips
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+function displayCategory() {
+  // Dynamically obtain categories
+  let projectCategories = projects.projectCategories.map(x => x.name);
+  let activeClasses = 'nav-link active my-5 my-lg-0 py-3 text-lg-left text-center';
+  let inactiveClasses = 'nav-link my-5 my-lg-0 py-3 text-lg-left text-center';
+
+  for(let i of projectCategories){
+    let temp_id = i.toLowerCase().replace(" ", "-");
+    let temp_name = i;
+    let navTabTemplate = `<span
+      class="nav-link my-5 my-lg-0 py-3 text-lg-left text-center"
+      id="v-tabs-${temp_id}-tab"
+      data-toggle="pill"
+      href="#v-tabs-${temp_id}"
+      role="tab"
+      aria-controls="v-tabs-${temp_id}"
+      aria-selected="false">
+        ${temp_name}
+      </span>`;
+    $("#v-tabs-tab-projects").append(navTabTemplate);
+  }
+
+  $("#v-tabs-tab-projects > *").click(e => {
+    displayProjects(e.target.innerHTML);
+    displayProjectsCategory(e.target.innerHTML);
+  });
+}
 
 // read from projects.JSON
 var projects;
@@ -25,6 +38,8 @@ $.ajax({
   dataType: "json",
   success: function (data) {
     projects = data;
+    displayCategory();
+
     selectedProjects = projects.projects.filter(x => true);
 
     let currentQueries = getQueryObj();
@@ -42,6 +57,7 @@ $.ajax({
 });
 
 function displayProjectsCategory(targetCategory) {
+  targetCategory = targetCategory.trim();
   let project = projects.projectCategories.filter(x => x.name == targetCategory)[0];
   let description = project.desc;
   $("#project-header-title").html(targetCategory);
@@ -50,6 +66,7 @@ function displayProjectsCategory(targetCategory) {
 }
 
 function displayProjects(targetCategory) {
+  targetCategory = targetCategory.trim();
   let projs = projects.projects.filter(x => x.category == targetCategory);
   if (targetCategory == "All Projects") {
     projs = projects.projects.filter(x => true);
@@ -89,8 +106,3 @@ function displayProjects(targetCategory) {
     $("#projects").append(template);
   }
 }
-
-$("#v-tabs-tab-projects > *").click(e => {
-  displayProjects(e.target.innerHTML);
-  displayProjectsCategory(e.target.innerHTML);
-});
