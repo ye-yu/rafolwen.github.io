bc824aa6659d45ad49bd81e0b6b924478bb374f9
ye-yu <rafolwen98@gmail.com>
2019-12-27 09:09:41 +0800

    Update target project md url

--- js/blog/projects.js
+++ js/blog/projects.js
@@ -3,7 +3,7 @@ let currentQueries = getQueryObj();
 if (!Object.keys(currentQueries).length < 1) {
   if (currentQueries.project != undefined) {
       // Content placeholder
-      let projectSrc = `blog/content/${currentQueries.project}.md`;
+      let projectSrc = `js/blog/content/${currentQueries.project}.md`;
       console.log(`Searching for ${projectSrc}`);
       $("#projects").empty();
   }
