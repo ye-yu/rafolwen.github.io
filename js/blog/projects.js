029caab4d560eb8fba88387c00e937ebe39fcf38
ye-yu <rafolwen98@gmail.com>
2019-12-27 09:26:32 +0800

    Fix indentation in projects.js

--- js/blog/projects.js
+++ js/blog/projects.js
@@ -2,21 +2,21 @@ var md = new Remarkable();
 let currentQueries = getQueryObj();
 if (!Object.keys(currentQueries).length < 1) {
   if (currentQueries.project != undefined) {
-      // Content placeholder
-      let projectSrc = `js/blog/content/${currentQueries.project}.md`;
-      $("#projects").empty();
+    // Content placeholder
+    let projectSrc = `js/blog/content/${currentQueries.project}.md`;
+    $("#projects").empty();
 
-      // fetch md content
-      $.ajax({
-        url: projectSrc,
-        contentType: "text/plain",
-        success: function (data) {
-          $("#project-header-title").empty();
-          $("#project-header-subtitle").html(`Project ID: ${currentQueries.project.slice(0, 6)}`);
-          $("#projects").append(`
-            <div class="project-markdown">
-              ${md.render(data)}
-            </div>`);
+    // fetch md content
+    $.ajax({
+      url: projectSrc,
+      contentType: "text/plain",
+      success: function (data) {
+        $("#project-header-title").empty();
+        $("#project-header-subtitle").html(`Project ID: ${currentQueries.project.slice(0, 6)}`);
+        $("#projects").append(`
+          <div class="project-markdown pt-5">
+          ${md.render(data)}
+          </div>`);
         },
         error: function (e, status, errStr) {
           console.log(e);
@@ -24,5 +24,5 @@ if (!Object.keys(currentQueries).length < 1) {
           console.log(errStr);
         }
       });
+    }
   }
-}
