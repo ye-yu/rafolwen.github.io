c104719734398f735452a04618953604d988f728
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-04-15 19:35:45 +0800

    Add stylesheet

---
+++ src/styles/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const stylesheet = {
+  ColumnFlex: {
+    display: "flex",
+    flexDirection: "column",
+  } as React.CSSProperties,
+  FlexItemGrow: {
+    flex: 1,
+  } as React.CSSProperties,
+}
+
+export default stylesheet
