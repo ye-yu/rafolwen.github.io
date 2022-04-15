6108554f62ba1447ec8d986e7439470fe45b5ec6
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-04-15 16:21:52 +0800

    Add store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './stores';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+if (container === null) {
+  console.error("No element with id = 'root' is in the page!")
+} else {
+  const root = createRoot(container);
+  root.render(<ContextProvider><App /></ContextProvider>);  
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
