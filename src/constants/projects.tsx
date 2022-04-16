4b01fc8a0b8c0732d97c3ab644ee68ccb1842263
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-04-16 19:50:52 +0800

    Add backend projects

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -10,47 +10,49 @@ export const project = {
         </p>
         <p>
           Projects related to backend development:
-          <ol>
+          <ul>
             <li>User management module</li>
             <li>Microservices</li>
             <li>Worker applications</li>
-          </ol>
+          </ul>
         </p>
       </div>,
     },
     {
-      title: "Restful API",
-      pills: ["express.js", "typescript"],
+      title: "Chatbot",
+      pills: ["typescript"],
       content: <div>
         <p>
-          Sequelize as the ORM, Redis for hot data storage, CRUD api design, Joi for schema validation.
-          Using intuitive versioning and route naming to ensure long term maintainability.
+          Using npm package <code>node-nlp</code> to perform keyword entity extraction and basic neural network to
+          respond to user free text. Application design is perfect for frequently asked questions and business flows
+          that is keyword-sensitive.
         </p>
         <p>
-          Projects related to backend development:
-          <ol>
-            <li>User management module</li>
-            <li>Microservices</li>
-            <li>Worker applications</li>
-          </ol>
+          Optimisations include:
+          <ul>
+            <li>
+              text filtering with synonyms dictionaries
+            </li>
+            <li>
+              action-object groupings
+            </li>
+          </ul>
         </p>
       </div>,
     },
     {
-      title: "Restful API",
-      pills: ["express.js", "typescript"],
+      title: "NodeBB Forums",
+      pills: ["node.js"],
       content: <div>
         <p>
-          Sequelize as the ORM, Redis for hot data storage, CRUD api design, Joi for schema validation.
-          Using intuitive versioning and route naming to ensure long term maintainability.
+          Customising nodebb as a general purpose forum server, and developing plugins using Typescript 
+          for highest type-safeness.
         </p>
         <p>
-          Projects related to backend development:
-          <ol>
-            <li>User management module</li>
-            <li>Microservices</li>
-            <li>Worker applications</li>
-          </ol>
+          Plugin includes:
+          <ul>
+            <li>Authentication integration with Auth0</li>
+          </ul>
         </p>
       </div>,
     },
