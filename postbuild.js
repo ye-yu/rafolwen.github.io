3b9a5692ca16cdd87ae9c54e9e01f7c818bf6067
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-04-16 21:32:03 +0800

    Prevent image minify on build

--- postbuild.js
+++ postbuild.js
@@ -1,36 +1,16 @@
-import imagemin from 'imagemin'
-import imageminJpegtran from 'imagemin-jpegtran'
 import * as fs from 'fs'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
+import { exec } from 'child_process'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const buildPath = path.join(__dirname, "build/static/media")
+const buildPath = path.join(__dirname, "build")
 
 if (!fs.existsSync(buildPath)) {
   console.error(":: No build directory is found. Exiting!")
   process.exit(1)
 }
 
-
-imagemin(
-  [path.join(buildPath, "*.jpg")],
-  {
-    destination: buildPath,
-    plugins: [
-      imageminJpegtran({
-        arithmetic: true,
-        progressive: true,
-      }),
-    ]
-  }
-).then(result => {
-  console.log(":: Minified files:")
-  result.forEach(file => console.log("::    -", file.destinationPath))
-  process.exit()
-}).catch(err => {
-  console.error(":: Error at imagemin:", err)
-  process.exit(1)
-})
+exec("scp -r build centos@51.79.248.35:/home/centos/raflie.cc")
