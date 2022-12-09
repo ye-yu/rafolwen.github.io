8128cf07cd3e361daeb34324726d4db50a6b5640
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-12-09 14:19:22 +0800

    Use craco and esbuild for dev

--- src/hooks/useBreakpoint.tsx
+++ src/hooks/useBreakpoint.tsx
@@ -1,8 +1,15 @@
-import useBP from 'use-breakpoint';
+import useBP from "use-breakpoint";
 
-export const BREAKPOINTS = { mobile: 400, tablet: 900, desktop: 1280 }
+export const BREAKPOINTS = { mobile: 400, tablet: 900, desktop: 1280 };
 
 export default function useBreakpoint() {
-  const vpWidth = window.visualViewport.width
-  return useBP(BREAKPOINTS, vpWidth > BREAKPOINTS.desktop ? "desktop" : vpWidth > BREAKPOINTS.tablet ? "tablet" : "mobile");
+  const vpWidth = window.visualViewport?.width ?? 0;
+  return useBP(
+    BREAKPOINTS,
+    vpWidth > BREAKPOINTS.desktop
+      ? "desktop"
+      : vpWidth > BREAKPOINTS.tablet
+      ? "tablet"
+      : "mobile"
+  );
 }
