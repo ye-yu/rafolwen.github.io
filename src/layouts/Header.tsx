4746112379e20ed7785a18c44d3b1ce547660864
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-15 23:28:56 +0800

    Add resume link below name

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -23,20 +23,54 @@ function Header() {
   return (
     <div style={stylesheet.ColumnFlex}>
       {/* TITLE */}
-      <div>
+      <div style={stylesheet.FlexItemGrow}>
+        <Display style={stylesheet.CenteredResponsive(breakpoint)}>
+          Raflie Zainuddin
+        </Display>
+      </div>
+      <div style={stylesheet.RowFlex}>
         <button
+          style={{
+            padding: "5px 10px",
+            letterSpacing: "0.1rem",
+            marginTop: "-1rem",
+            marginBottom: "2rem",
+            ...stylesheet.RowFlex,
+          }}
+          className={`button-${mode}`}
           onClick={() =>
             (window.location.href =
               "/resumes/Raflie Zainuddin - Backend Developer March 2023.pdf")
           }
         >
-          Resume
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="feather feather-file-text"
+          >
+            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+            <polyline points="14 2 14 8 20 8"></polyline>
+            <line x1="16" y1="13" x2="8" y2="13"></line>
+            <line x1="16" y1="17" x2="8" y2="17"></line>
+            <polyline points="10 9 9 9 8 9"></polyline>
+          </svg>{" "}
+          <span
+            style={{
+              marginLeft: "0.5rem",
+              marginTop: "auto",
+              marginBottom: "auto",
+            }}
+          >
+            RESUME
+          </span>
         </button>
-      </div>
-      <div style={stylesheet.FlexItemGrow}>
-        <Display style={stylesheet.CenteredResponsive(breakpoint)}>
-          Raflie Zainuddin
-        </Display>
       </div>
       <div style={stylesheet.FlexItemGrow}>
         <div
