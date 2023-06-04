ec5652fc9eb1dd0d498592c889528bb8df6dd1f9
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-06-04 23:42:24 +0800

    Add theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -191,16 +191,7 @@ function App() {
                   margin: 0,
                   display: breakpoint === "mobile" ? "none" : undefined,
                 }}
-              >
-                <div
-                  className="header-theme"
-                  onClick={() =>
-                    appState.setTheme(theme === "light" ? "dark" : "light")
-                  }
-                >
-                  <div className={theme ?? "light"}></div>
-                </div>
-              </li>
+              ></li>
             </ul>
           </div>
         </div>
@@ -369,6 +360,54 @@ function App() {
               </div>
             ))}
           </div>
+        </div>
+      </div>
+      <div className="theme-switch">
+        <div
+          // className="header-theme"
+          onClick={() =>
+            appState.setTheme(theme === "light" ? "dark" : "light")
+          }
+        >
+          {theme === "light" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="yellow"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-sun"
+            >
+              <circle cx="12" cy="12" r="5"></circle>
+              <line x1="12" y1="1" x2="12" y2="3"></line>
+              <line x1="12" y1="21" x2="12" y2="23"></line>
+              <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+              <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+              <line x1="1" y1="12" x2="3" y2="12"></line>
+              <line x1="21" y1="12" x2="23" y2="12"></line>
+              <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+              <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="white"
+              stroke="white"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-moon"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          )}
         </div>
       </div>
     </div>
