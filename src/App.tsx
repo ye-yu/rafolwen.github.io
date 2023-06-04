9975048d4ad9b135fa04873b77dc0e1d2bba87e6
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-06-04 21:20:55 +0800

    Add about section

--- src/App.tsx
+++ src/App.tsx
@@ -188,7 +188,49 @@ function App() {
       <div className="spacing"></div>
       <div className="section about" id="about">
         <div className="section-name">about</div>
-        <div className="content">content</div>
+        <div className="content">
+          <div className="hello">
+            Hi, I am a software engineer,
+            <br />
+            and I do a lot of{" "}
+            <span style={{ fontWeight: "bold" }}>Backend Development</span> .
+          </div>
+          <div className="description">
+            I enjoy digitizing business workflows into automated services
+            through API servers, scheduled jobs, and optimizations.
+          </div>
+          <div className="cta">
+            <a className="button primary" href="mailto:contact@raflie.cc">
+              <div>contact@raflie.cc</div>
+            </a>
+            <a
+              className="button secondary"
+              href="/resumes/Raflie Zainuddin - Backend Developer March 2023.pdf"
+            >
+              <div className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-file-text"
+                >
+                  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                  <polyline points="14 2 14 8 20 8"></polyline>
+                  <line x1="16" y1="13" x2="8" y2="13"></line>
+                  <line x1="16" y1="17" x2="8" y2="17"></line>
+                  <polyline points="10 9 9 9 8 9"></polyline>
+                </svg>
+              </div>
+              <div>resume</div>
+            </a>
+          </div>
+        </div>
       </div>
       <div className="section skillset" id="skillset">
         <div className="section-name">skillset</div>
